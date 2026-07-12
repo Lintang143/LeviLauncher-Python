ecbeeb1252a8bc568a3a264f372537a4aaf90492
Lintang143 <lintangsholeh860@gmail.com>
2026-07-12 09:55:56 +0700

    Update log messages for mods folder operations

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -24,12 +24,33 @@ bool ClangeMeMod::load() {
                                ec.message());
         return false;
     }
-
+    
     std::filesystem::create_directories(self.getConfigDir(), ec);
     if (ec) {
         self.getLogger().error("Failed to create config directory {}: {}",
                                self.getConfigDir().string(), ec.message());
         return false;
+    }
+
+    self.getLogger().info("[Logger] Mod Main Folder: {}", self.getModDir().string());
+    self.getLogger().info("[Logger] Mod Data Folder: {}", self.getDataDir().string());
+    self.getLogger().info("[Logger] Mod Config Folder: {}", self.getConfigDir().string());
+
+    std::filesystem::path modsPath = self.getDataDir() / "mods";
+    
+    // Cek apakah folder 'mods' sudah ada
+    if (std::filesystem::exists(modsPath)) {
+        self.getLogger().info("[INFO] 'mods' folder already exist in: {}. cannot create folder", modsPath.string());
+    } else {
+        self.getLogger().info("'mods' folder not exist. Creating folder in: {}", modsPath.string());
+        
+        std::filesystem::create_directories(modsPath, ec);
+        if (ec) {
+            self.getLogger().error("failed while making 'mods' folder:{}", ec.message());
+            // return false; // Buka komen ini 
+        } else {
+            self.getLogger().info("[SUKSES] Folder 'mods' berhasil dibuat untuk pertama kali!");
+        }
     }
 
     mConfigFile.emplace();
